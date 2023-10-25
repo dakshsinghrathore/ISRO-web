06b7e9c5cdd08fc32e9c2584863ddb5131e5cf61
AdityaGaur7 <adgaur027@gmail.com>
2023-10-25 19:15:27 +0530

    bottom to up added

--- JS/script.js
+++ JS/script.js
@@ -128,3 +128,21 @@ satelliteOption.addEventListener(("click"),()=>{
 })
 
 
+document.addEventListener("DOMContentLoaded", function() {
+  const scrollToTopButton = document.getElementById("scrollToTopButton");
+
+  window.onscroll = function() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          scrollToTopButton.style.display = "block";
+      } else {
+          scrollToTopButton.style.display = "none";
+      }
+  };
+
+  scrollToTopButton.addEventListener("click", function() {
+      window.scrollTo({
+          top: 0,
+          behavior: "smooth"
+      });
+  });
+});
